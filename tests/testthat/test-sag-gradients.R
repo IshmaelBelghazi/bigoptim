a08a82aba19cf55fbd4fed0d3064dfcce759c255
Toby Dylan Hocking <tdhock5@gmail.com>
2015-06-09 09:17:48 -0400

    rcv1 data crash example

--- tests/testthat/test-sag-gradients.R
+++ tests/testthat/test-sag-gradients.R
@@ -2,6 +2,23 @@ context("Gradient tests")
 
 ## test parameters
 eps <- 1e-08
+
+L2regularized.logistic.regression.gradient <- function(X, y, lambda, weight){
+  stop("TODO compute gradient in R code")
+}
+
+data(rcv1_train)
+
+test_that("gradient of rcv1", {
+  lambda <- 1
+  X <- as.matrix(rcv1_train$X)
+  pryr::mem_change({
+    fit <- sag_constant(X, rcv1_train$y, lambda)
+  })
+  gradient <- L2regularized.logistic.regression.gradient()
+  expect_less_than(sum(abs(gradient)), eps)
+})
+
 ## Simulating logistic datasets
 true_params <- c(1, 2, 3)
 sample_size <- 1000
@@ -11,16 +28,18 @@ sim <- .simulate_logistic(true_params, sample_size, intercept=FALSE)
 ## SAG with Constant Step Size ##
 #################################
 test_that("constant step size Sag gradient norm is zero", {
-    ## Fitting SAG
+  ## Fitting SAG
+  pryr::mem_change({
     sag_fit <- sag_constant(sim$X, sim$y, lambda=0, maxiter=NROW(sim$X) * 100)
-    expect_less_than(norm(sag_fit$d, type="F"), eps)
+  })
+  
+  expect_less_than(norm(sag_fit$d, type="F"), eps)
 })
 
 #########################
 ## SAG with linesearch ##
 #########################
 test_that("linesearch SAG gradient norm is zero", {
-
     expect_less_than(sag_ls(), eps)
 })
 ##########################################################
