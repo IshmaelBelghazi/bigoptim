4cb698aa975d916a4b9e6e0ef4d9e3dc99c76bd8
Ishmael Belghazi <ishar667@gmail.com>
2015-06-11 15:18:45 -0400

    Added R log-sum-exp. Started working on glm models

---
+++ R/glm_models.R
@@ -0,0 +1,19 @@
+## * Generalized Linear Models R function
+## ** Conditional Probabilities
+.get_glm_condprob <- function(X, y, W,
+                              activation=function(x) x,
+                              log_prob=FALSE,
+                              ...) {
+    stop("not implemented yet")
+}
+## ** Cost functions (negative log-likelihood)
+.get_glm_cost <- function(X, y, W, activation=function(x) x, ...) {
+    stop("not implemented yet")
+}
+## ** cost function gradient
+.get_glm_grad <- function(X, y, W, activation=function(x) x, ...) {
+    stop("not implemented yet")
+}
+
+
+
