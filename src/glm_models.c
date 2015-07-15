bb408ee05639a5928d5ac31dd165f75f2ef664ad
Mohamed Ishmael Diwan Belghazi <ishmael.belghazi@gmail.com>
2015-07-15 17:04:18 -0400

    Added Exponential and Poisson Losses and Gradients

--- src/glm_models.c
+++ src/glm_models.c
@@ -11,15 +11,40 @@ double binomial_loss(double y, double innerProd) {
 double binomial_grad(double y, double innerProd) {
   return -y/(1 + exp(y * innerProd));
 }
+
 /*=========\
 | GAUSSIAN |
 \=========*/
 /* loss function (not error function)*/
 double gaussian_loss(double y, double innerProd) {
-  return 0.5 * pow(innerProd - y, 2);
+  return 0.5 * (innerProd - y) * (innerProd - y);
 }
 /*Gradient of loss function*/
 double gaussian_grad(double y, double innerProd) {
   return innerProd - y;
 }
 
+/*============\
+| Exponential |
+\============*/
+/* Exponential loss function */
+double exponential_loss(double y, double innerProd) {
+  return exp(-y * innerProd);
+}
+/* Exponential gradient function */
+double exponential_gradient(double y, double innerProd) {
+  return -y * exp(-y * innerProd);
+}
+
+/*========\
+| Poisson |
+\========*/
+/* Poisson loss function */
+double poisson_loss(double y, double innerProd) {
+  return exp(innerProd) - y * innerProd;
+}
+/* Poisson gradient function */
+double poisson_grad(double y, double innerProd) {
+  return exp(innerProd) - y;
+
+}
--- src/glm_models.h
+++ src/glm_models.h
@@ -9,14 +9,23 @@ typedef struct {
   double (*grad)(double, double);
 } GlmModel;
 
-typedef enum {GAUSSIAN, BINOMIAL} GlmType; 
+typedef enum {GAUSSIAN,
+              BINOMIAL,
+              EXPONENTIAL,
+              POISSON
+} GlmType;
 
 /* Gaussian*/
-double gaussian_grad(double y, double innerProd);
 double gaussian_loss(double y, double innerProd);
-
+double gaussian_grad(double y, double innerProd);
 /* Binomial*/
-double binomial_grad(double y, double innerProd);
 double binomial_loss(double y, double innerProd);
+double binomial_grad(double y, double innerProd);
+/*  Exponential */
+double exponential_loss(double y, double innerProd);
+double exponential_grad(double y, double innerProd);
+/* Poisson */
+double poisson_loss(double y, double innerProd);
+double poisson_grad(double y, double innerProd);
 
 #endif /* GLM_MODELS_H_ */
