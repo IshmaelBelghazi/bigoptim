a921824783120e917c5a1ae0c4f980efa094b7d8
Mohamed Ishmael Diwan Belghazi <ishmael.belghazi@gmail.com>
2015-07-17 19:18:37 -0400

    Simulations for exponential and poisson models.

--- R/simulations.R
+++ R/simulations.R
@@ -1,5 +1,5 @@
 ##' @export
-.simulate_logistic <- function(true_params, sample_size, intercept=FALSE) {
+.simulate_bernoulli <- function(true_params, sample_size, intercept=FALSE) {
 
     n_params <- length(true_params)
     X <- matrix(rnorm(n_params * sample_size), ncol=n_params)
@@ -22,6 +22,34 @@
     }
     z <- X %*% true_params
     y <- z + rnorm(n=sample_size)
+    y <- matrix(y, ncol=1)
+    return(list(X=X, y=y, true_params=true_params))
+}
+
+##' @export
+.simulate_exponential <- function(true_params, sample_size, intercept=FALSE) {
+    n_params <- length(true_params)
+    X <- matrix(runif(n_params * sample_size), ncol=n_params)
+    if (intercept) {
+        X[, 1] <- 1
+    }
+    z <- X %*% true_params
+    pr <- -z
+    y <- rexp(sample_size, pr)
+    y <- matrix(y, ncol=1)
+    return(list(X=X, y=y, true_params=true_params))
+}
+
+##' @export
+.simulate_poisson <- function(true_params, sample_size, intercept=FALSE) {
+    n_params <- length(true_params)
+    X <- matrix(runif(n_params * sample_size), ncol=n_params)
+    if (intercept) {
+        X[, 1] <- 1
+    }
+    z <- X %*% true_params
+    pr <- exp(z)
+    y <- as.numeric(rpois(sample_size, pr))
     y <- matrix(y, ncol=1)
     return(list(X=X, y=y, true_params=true_params))
 }
