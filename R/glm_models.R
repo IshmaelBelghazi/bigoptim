b65ed0f8e0badaf91e2b78dfae57ebc3cbe779ad
Mohamed Ishmael Diwan Belghazi <ishmael.belghazi@gmail.com>
2015-07-20 11:42:47 -0400

    Added regularization to R GLM gradients.

--- R/glm_models.R
+++ R/glm_models.R
@@ -2,22 +2,22 @@
 ## ** Gradient functions
 ## *** Gaussian
 ##' @export
-.gaussian_grad <- function(X, y, w) {
-  ((X %*% w) - y)/NROW(X) 
+.gaussian_grad <- function(X, y, w, lambda=0) {
+  ((X %*% w - y) + lambda * sum(w))/NROW(X) 
 }
 ## *** Bernoulli
 ##' @export
-.bernoulli_grad <- function(X, y, w) {
-  (-y/(1 + exp(y * (X %*% w))))/NROW(X)
+.bernoulli_grad <- function(X, y, w, lambda=0) {
+  ((-y/(1 + exp(y * (X %*% w)))) + lambda * sum(w))/NROW(X)
 }
 
 ## *** Exponential
 ##' @export
-.exponential_grad <- function(X, y, w) {
-  (-y * exp(y * (X %*% w)))/NROW(X)
+.exponential_grad <- function(X, y, w, lambda=0) {
+  ((-y * exp(y * (X %*% w))) + lambda * sum(w))/NROW(X)
 }
 ## *** Poisson
 ##' @export
-.poisson_grad <- function(X, y, w) {
-  (exp(X %*% w) - y)/NROW(X)
+.poisson_grad <- function(X, y, w, lambda=0) {
+  ((exp(X %*% w) - y) + lambda * sum(w))/NROW(X)
 }
